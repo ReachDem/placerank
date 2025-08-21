287eacb0b0be47eea41058fbf102f8dd963cfeaf
Belrick Stephane <73307766+latioms@users.noreply.github.com>
2025-08-22 00:44:32 +0100

    feat(auth): add phone number plugin for OTP in authentication flow
    feat(prisma): update User model to include phone number fields

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,13 +1,20 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { phoneNumber } from "better-auth/plugins"
 // If your Prisma file is located elsewhere, you can change the path
 import { PrismaClient } from "@/lib/generated/prisma";
- 
+
 const prisma = new PrismaClient();
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
 
-    
+    plugins: [
+        phoneNumber({
+            sendOTP: ({ phoneNumber, code }, request) => {
+                // Implement sending OTP code via SMS
+            }
+        })
+    ]
 });
